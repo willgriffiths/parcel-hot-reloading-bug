e12235d33fcb94a4d08283eb42ecfdef87e70c8d
Will Griffiths <will.griffiths@fusionsport.com>
2021-09-22 10:02:26 +1000

    Fix hot reloading

--- packages/hero/src/App.tsx
+++ packages/hero/src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const App = () => {
   return (
     <p>
-      Hello world! Edit and save this text and it won't
+      Hello world! Edit and save this text and it WILL
       hot reload.
     </p>
   );
